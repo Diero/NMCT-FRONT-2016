51654a57dec1c0e42e8e3d8121e18c124e2c26b8
kevinderudder <kevin_derudder@hotmail.com>
2016-10-05 14:26:28 +0200

    week 2 commit

---
+++ W2/Demo/app/js/main.js
@@ -0,0 +1,54 @@
+/**
+ * Created by kevinderudder on 05/10/16.
+ */
+(function(){
+    "use strict";
+
+
+    var searchButton, filter, imagesList;
+
+    function renderImages(images){
+        // <li></li>
+        var bobTheHTMLBuilder = "",
+            l = images.photos.photo.length;
+
+        for(var i = 0 ; i < l ; i++){
+            var o = images.photos.photo[i];
+            bobTheHTMLBuilder += '<li class="media">';
+            bobTheHTMLBuilder += '<img src="'+parseUrl(o)+'" class="media__element" />'
+            bobTheHTMLBuilder += '</li>';
+        }
+
+        imagesList.innerHTML = bobTheHTMLBuilder;
+    }
+
+    function parseUrl(flickrObject){
+        return "https://farm"+flickrObject.farm+".staticflickr.com/"+flickrObject.server+"/"+flickrObject.id+"_"+flickrObject.secret+".jpg";
+    }
+
+    function handleError(error){
+        console.error(error);
+    }
+    function loadImages(){
+        var searchText = filter.value;
+        var url = "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=dd7adaf769d707a0230f663c608827d0&tags="+searchText+"&format=json&nojsoncallback=1";
+
+        $.UI.getData(url).then(renderImages, handleError);
+    }
+
+
+    function addEventListeners(){
+        searchButton.addEventListener("click", loadImages);
+    }
+
+    function init(){
+        searchButton = document.getElementById("searchButton");
+        filter = document.getElementById("filter");
+        imagesList = document.querySelector(".image__list")
+
+        addEventListeners();
+    }
+    init();
+
+
+})();
